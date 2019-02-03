3d3ad8dfea8e703f1d9d3dca0474f42aa9489e25
Nelson Rodrigues <nelson@waveweb2.com>
2019-02-03 13:38:47 +0000

    Updates

---
+++ precache-manifest.cab88dfbe8fe00a43bfd164afbb7869d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17401247161485f67fff",
+    "url": "/mtg-app/static/css/main.5b37fb77.chunk.css"
+  },
+  {
+    "revision": "17401247161485f67fff",
+    "url": "/mtg-app/static/js/main.17401247.chunk.js"
+  },
+  {
+    "revision": "95b6122b82b3f997ff1f",
+    "url": "/mtg-app/static/js/1.95b6122b.chunk.js"
+  },
+  {
+    "revision": "024d289d0e052b667bd4",
+    "url": "/mtg-app/static/js/runtime~main.024d289d.js"
+  },
+  {
+    "revision": "e587e9d0e402041c96e064288df9ea8c",
+    "url": "/mtg-app/index.html"
+  }
+];
